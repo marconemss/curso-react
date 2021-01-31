4415b6a94fe465ad8c8c48508f7919c292a1e245
Marcone Silva <marconemss@gmail.com>
2021-01-31 17:19:29 -0300

    Tópico 59 - Hooks - useState

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
+
+    /**
+     * Esta forma de declarar o array acima é a mesma coisa de dizer:
+     * 
+     * const count = array[0] 
+     * const setCount = array[1] 
+     * 
+     * No caso acima, ele inicializa count = 0;
+     * 'setCount' é o nome da função utilizada abaixo e definida na arrow function.
+     */
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+                subtitle="Estado em componentes funcionais!" />
+
+            <SectionTitle title="Exercício 01" />
+            <div className="center">
+                <span className="text"> {count} </span>
+                <div>
+                    <button className="btn" onClick={() => setCount(count - 1)}> -1 </button>
+                    <button className="btn" onClick={() => setCount(count + 1)}> +1 </button>
+                    <button className="btn" onClick={() => setCount(500)}> 500 </button>
+                    <button className="btn" onClick={() => setCount(currect => currect + 500)}> +500 </button>
+                </div>
+            </div>
+
+
+            <SectionTitle title="Exercício 02" />
+            <input type="text" className="input" 
+                    value={name} onChange={e => setName(e.target.value)} />
+
+            <span>{name}</span>
         </div>
     )
 }
