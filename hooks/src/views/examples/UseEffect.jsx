c543350298d716da8f1d442442229fca1ef28610
Marcone Silva <marconemss@gmail.com>
2021-01-31 17:54:20 -0300

    Tópico 60 - Hooks - useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFatorial(num){
+    const n = parseInt(num)
+    if(n < 0) return -1
+    if(n === 0) return 1
+    return calcFatorial(n-1) * n
+}
 
 const UseEffect = (props) => {
+
+    const [number, setNumber] = useState(1)
+    const [fatorial, setFatorial] = useState(1)
+
+    useEffect(function () {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    /**
+     * Acima: sempre que [number] tiver o seu estado alterado,
+     * vai gerar um efeito colateral definido por function():
+     * invoca o setFatorial (isto é, mudança no estado de number
+     * está mudando o estado de fatorial) que, por sua vez,
+     * invoca a função recursiva calcFatorial definida lá em cima!
+     */
+
+    useEffect(function () {
+        if (number > 18){
+            document.title = 'Eita!!!!!';
+        } else {
+            document.title = 'React App';
+        }
+    }, [number])
+
     return (
-        <div className="UseEffect">
+        <div>
             <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
+                    title="Hook UseEffect"
+                    subtitle="Permite executar efeitos colaterais em componentes funcionais!" />
+
+            <SectionTitle title="Exercício 01" />
+            <div className="UseEffect">
+                <div>
+                    <div>
+                        <span className="text">Fatorial:</span>
+                        <span className="text red">{fatorial < 0 ? 'Não existe' : fatorial}</span>
+                    </div>
+                    <input type="number" className="text" value={number} 
+                            onChange={e => setNumber(e.target.value)} />
+                </div>
+            </div>
+            <p />
+            <SectionTitle title="Exercício 02" />
+
         </div>
     )
 }
